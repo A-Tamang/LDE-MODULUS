ce07cdae9c79a3c0c25ffc1e14fc8c5db5662615
Ashish Tamang <waiba_ashish@hotmail.com>
2015-01-08 21:45:34 -0500

    Added Info

--- Project.py
+++ Project.py
@@ -5,15 +5,39 @@
 # Student of University of Waterloo
 # Created: Winter 2014
 #
-# Future updates : Particular Solution Add-on
-#                : Will be able to accept negative Modulo
-#                : Make modulo more efficient by simplying before
-#                  evaluating via EEA Algorithm
-#                : MAIN UPDATE: Make into webpage 
+# Synopsis:
+#          
+#          A text-based Dungeon Crawling RPG made in Python. It employs the use of
+#          Object Oriented Programming to handle stats of the characters and monsters.
+#          Careful attention has been paid to balancing the game to make the playing
+#          experience as pleasurable as possible.
 #
-# Purpose:   This program can evaluate any LDE (Linear Diophantine Equations
-#                and Modulus Equations. It is fast and easy to use. To use,
-#                simply run the program (f5) and follow the instructions.
+# Description:
+#
+#          In BLT_RPG, you are an adventure raiding a floating castle inhabited by 
+#          the notorious bandit guild, 'Neko Hooligans'. In the character creation section, 
+#          you must pick one of five classes: 
+#          
+#               i) Berserker ii) Hemomancer iii) Weaponmaster iv) Archmage v) Lone Wolf
+#          
+#          With a weapon in hand, your objective is cleave through the Hooligans, ascend
+#          the three floors of the floating castle and confront the ever-elusive 
+#          phantom leader of the guild.
+#
+#
+# Instructions:
+#
+#          The game is very easy to play. It is text-based and therefore every interaction,
+#          --whether it is traversing through the dungeon, fighting monsters 
+#          or checking one's stats-- is done through the use of I/O with Python. 
+#
+#
+# Possible Future Updates:
+#
+#          i) Cleaner Battles; easier to distinguish damage
+#         ii) The integration of a map to make traversing a bit easier
+#        iii) The introduction of a GUI
+#
 #
 #-------------------------------------------------------------------------------------------
 import math
